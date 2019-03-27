1c8a22c521448ff5d2b18c01b8ac9d74e3463ac2
chentao.lct <chentao.lct@alibaba-inc.com>
2019-03-27 18:30:26 +0800

    feature#12341234#修改

---
+++ example_flutter/lib/business_logic/property_bloc.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_visual_builder/business_logic/server/generated/server.pb.dart';
+import 'package:example_flutter/business_logic/client/client.dart';
+import 'package:rxdart/rxdart.dart';
+
+class PropertyBloc {
+
+  // TODO implement receiving widget removed events
+  PropertyBloc._({this.editor, this.removedIds});
+
+  factory PropertyBloc(VisualClient visualClient) {
+
+    PublishSubject<String> removedIds = PublishSubject();
+
+    Observable<SelectedWidgetWithProperties> widgetStream = Observable(visualClient.serverClient.streamSelected(SelectStream())
+        .asBroadcastStream());
+
+    return PropertyBloc._(
+      editor: widgetStream,
+      removedIds: removedIds,
+    );
+  }
+
+  final Observable<SelectedWidgetWithProperties> editor;
+
+  final Sink<String> removedIds;
+
+
+}
